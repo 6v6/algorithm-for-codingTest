6cd46617c3ab65fb57d6eb4ab18c1c9a86364f14
injeong <injong032@gmail.com>
2020-05-27 19:57:36 +0900

    #10430 나머지 #2609 최대공약수와최소공배수

---
+++ baekjoon/최대공약수와최소공배수_2609.java
@@ -0,0 +1,29 @@
+package ch1;
+
+import java.util.Scanner;
+
+public class 최대공약수와최소공배수_2609 {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		int result1 = gcd(a,b);
+		int result2 = (a*b)/result1;
+		
+		System.out.println(result1);
+		System.out.println(result2);
+	}
+
+	public static int gcd(int a, int b) {
+		while (b != 0) {
+			int r = a % b;
+			a = b;
+			b = r;
+		}
+		return a;
+	}
+	
+
+}
