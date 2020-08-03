012c9758468d6658642e9fbb8746f78166aa2e48
Injeong <injeong410@gmail.com>
2020-08-03 17:51:56 +0900

    Create boj1110.java

---
+++ algorithm-for-codingTest/src/baekjoon/step/boj1110.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int number = n;
+        int ten, one; // 10의자리, 1의자리
+        int newNum;
+        int count = 0;
+
+        while (true) {
+            count++;
+            if (number < 10) {
+                ten = 0;
+                one = number;
+            } else {
+                ten = number / 10;
+                one = number % 10;
+            }
+
+            newNum = one * 10 + (ten + one) % 10;
+
+            if (newNum == n) {
+                System.out.println(count);
+                break;
+            }
+            number = newNum;
+        }
+    }
+}
