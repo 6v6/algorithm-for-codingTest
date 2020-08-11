b11b41aa44e295cdef2c4f12fc36c84fa9cfe39a
Injeong <injeong410@gmail.com>
2020-08-11 11:33:19 +0900

    Create 프린터.java

---
+++ algorithm-for-codingTest/src/programmers/stack+queue/프린터.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Documents{
+    int index;
+    int priority;
+    
+    public Documents(int index, int priority){
+        this.index = index;
+        this.priority = priority;
+    }
+}
+class Solution {
+
+    public int solution(int[] priorities, int location) {
+        Queue<Documents> qu = new LinkedList();
+        int count = 0;
+        int n = priorities.length;
+        int answer = 0;
+        
+        for(int i = 0; i < n; i++){
+            Documents dc = new Documents(i, priorities[i]);
+            qu.add(dc);
+        }
+        
+        while(true){
+            Documents target = qu.peek();
+            count = 0;
+            for(Documents dc : qu){
+                count++;
+                if(target.priority < dc.priority){
+                    qu.poll();
+                    qu.add(target);
+                    break;
+                }
+            }
+            if(count == qu.size()){
+                answer++;
+                qu.poll();
+                if(target.index == location)
+                    return answer;
+            }
+        }
+        
+       //return answer;
+    }
+    
+    
+}
