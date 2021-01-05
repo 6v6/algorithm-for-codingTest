e32b62170aa8c8078be328ac1cf7104487990d79
injeong <injeong410@gmail.com>
2021-01-06 02:56:01 +0900

    [pro] 조이스틱

---
+++ algorithm-for-codingTest/src/Greedy/joystick.java
@@ -0,0 +1,35 @@
+package Greedy;
+
+public class joystick {
+	public int joystick(String name) {
+        int result = 0;
+        int length = name.length();
+        
+        //알파벳 변환 최소
+        for(int i =  0; i < length; i++){
+            Character target = name.charAt(i);
+            if(target != 'A'){
+               if(target - 'A' <= 'Z'-target+1) 
+                   result += target - 'A';
+                else
+                    result += 'Z'-target+1;     
+            }
+        }
+               
+        //이동 값 최소
+        int minMove = length;
+        for(int i =  0; i < length; i++){
+            if(name.charAt(i) != 'A'){
+                int next = i+1;
+                while(next < length && name.charAt(next) == 'A'){
+                    next++;
+                }
+                int move = 2 * i + length - next;
+                if(minMove > move)
+                    minMove = move; 
+            }
+        }
+        result += minMove;
+        return result;
+    }
+}
