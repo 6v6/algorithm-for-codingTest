a0e0338c295fc4357f0831b2df6e24cf443e1d84
6v6 <injeong410@gmail.com>
2021-01-20 22:44:36 +0900

    줄바꿈 수정

--- algorithm-for-codingTest/src/DP/integerTriangle.java
+++ algorithm-for-codingTest/src/DP/integerTriangle.java
@@ -2,16 +2,16 @@ package DP;
 
 //정수 삼각형
 public class integerTriangle {
-	
-	//bottom-up방식으로 풀이
-	//삼각형 밑에서부터 큰 값을 선택하여 update되도록  함.
+
+	// bottom-up방식으로 풀이
+	// 삼각형 밑에서부터 큰 값을 선택하여 update되도록 함.
 	public int solution(int[][] triangle) {
 		for (int i = triangle.length - 2; i >= 0; i--) {
 			for (int j = 0; j < triangle[i].length; j++)
 				triangle[i][j] += Math.max(triangle[i + 1][j], triangle[i + 1][j + 1]);
 		}
-		
-		//꼭대기에 최대값에 합이 계산됨
+
+		// 꼭대기에 최대값에 합이 계산됨
 		return triangle[0][0];
 	}
 
