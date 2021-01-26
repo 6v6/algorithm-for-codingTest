472087d237aa2c26c0a315d2177445b1c8cf3fde
6v6 <injeong410@gmail.com>
2021-01-26 22:51:40 +0900

    [pro] 43238+입국심사

---
+++ algorithm-for-codingTest/src/programmers/pro43238.java
@@ -0,0 +1,33 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class pro43238 {
+	public long solution(int number, int[] times) {
+		long answer = 1000000000;
+		Arrays.sort(times);
+
+		long n = (long) number;
+		long left = 0;
+		long right = (long) times[times.length - 1] * n;
+
+		while (left <= right) {
+			long mid = (left + right) / 2;
+
+			long sum = 0;
+			for (long time : times) {
+				sum += mid / time;
+			}
+
+			if (sum >= n) {
+				answer = mid;
+				right = mid - 1;
+			} else {
+				left = mid + 1;
+			}
+
+		}
+
+		return answer;
+	}
+}
